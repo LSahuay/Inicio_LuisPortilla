050639417d8c2b3b09fe13ef296104aaeb9f54f8
luisj <luisj@LuisPc>
2024-04-24 11:55:21 +0200

    Proyecto Inicial

---
+++ src/practica_clonar/PracClonar.java
@@ -0,0 +1,20 @@
+package practica_clonar;
+
+import java.util.Scanner;
+
+public class PracClonar {
+
+	public static void main(String[] args) {
+		
+		Scanner ent= new Scanner(System.in);
+		String frase = "";
+		
+		System.out.println("Introduce tu frase:");
+		frase = ent.nextLine();
+		
+		for(int i = frase.length()-1; i>=0; i--) {
+			System.out.print(frase.charAt(i));
+		}
+
+	}
+}
